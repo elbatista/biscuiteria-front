ef567fe2caa3906f57eb6806a226d31878c7146a
Elia <o.elia.batista@gmail.com>
2026-04-26 13:27:23 +0200

    enable cache

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  cacheComponents: true,
   /* config options here */
   images: {
     // unoptimized: process.env.NODE_ENV === "development",
